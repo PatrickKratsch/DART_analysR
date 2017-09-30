40d3ed9d4061b228ea6f284974f90496cd135be3
Patrick Kratschmer <Patrick.Kraetschmer@gmail.com>
2017-09-30 17:57:21 +0100

    Use data.table to create new functions and measures

--- DART_activity_overall.R
+++ DART_activity_overall.R
@@ -1,16 +1,11 @@
-DART_activity_overall <- function(tidy.clean, geno_names, ...){
+DART_activity_overall <- function(tidy.clean, cell_length){
   
   library(tidyr)
   library(data.table)
   library(dplyr)
   
-  genotypes <- list(...)
+  experiment_length <- cell_length * dim(tidy.clean)[1] / 60
   
-  i <- 1
-  for(genotype in genotypes){
-      
-    geno.data <- tidy.clean[, genotype := ifelse(fly %in% genotype, geno_names[i], "undefined")]
-    
-    i <- i + 1
-  }
+  activity_per_minute_overall <- tidy.clean[, .(total_activity = sum(activity) / ), by = fly]
+  
 }
